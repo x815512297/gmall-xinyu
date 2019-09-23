c5a3796723492c414108c5c267cfdd3b18065820
xinyu <815512297@qq.com>
2019-09-23 21:16:56 +0800

    谷粒商城3.0

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/CategoryServiceImpl.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/CategoryServiceImpl.java
@@ -1,21 +1,28 @@
 package com.atguigu.gmall.pms.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.atguigu.core.bean.PageVo;
 import com.atguigu.core.bean.Query;
 import com.atguigu.core.bean.QueryCondition;
-
 import com.atguigu.gmall.pms.dao.CategoryDao;
 import com.atguigu.gmall.pms.entity.CategoryEntity;
 import com.atguigu.gmall.pms.service.CategoryService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
+
+    @Autowired
+    private  CategoryDao categoryDao;
+
+
+
 
     @Override
     public PageVo queryPage(QueryCondition params) {
@@ -25,6 +32,23 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageVo(page);
+    }
+
+    @Override
+    public List<CategoryEntity> queryCategoryByCidOrlevel(Integer level, Long parentCid) {
+
+        QueryWrapper<CategoryEntity> queryWrapper = new QueryWrapper<>();
+        //判断level是否为0
+        if(level!=0){
+            queryWrapper.eq("cat_level",level);
+        }
+        //判断父节点的id是否为null
+        if (parentCid != null) {
+            queryWrapper.eq("parent_cid",parentCid);
+        }
+        //执行查询
+        List<CategoryEntity> categoryEntities = this.categoryDao.selectList(queryWrapper);
+        return categoryEntities;
     }
 
 }
